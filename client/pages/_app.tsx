274f56d438e5a5f1d450cf104439316a76625720
Shakiba448 <shakiba448@gmail.com>
2021-10-07 18:38:12 +0600

    WIP ON ADVANCE FILTER

---
+++ client/pages/_app.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import theme from "../src/Theme/theme";
+import createEmotionCache from "../src/Theme/createEmotionCache";
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: MyAppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
