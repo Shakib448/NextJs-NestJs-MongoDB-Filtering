08b01ab7c0b12aa100e2df9dcad21327bdf2b1fb
Shakiba448 <shakiba448@gmail.com>
2021-10-09 14:46:35 +0600

    Final Deploy

--- server/src/orders/orders.controller.ts
+++ server/src/orders/orders.controller.ts
@@ -4,18 +4,19 @@ import { OrderService } from './order.service';
 @Controller('orders')
 export class OrderController {
   constructor(private orderService: OrderService) {}
+
   @Post('insert')
   async create() {
     this.orderService.createOrders();
   }
 
   @Delete('delete')
-  async deleteCats() {
+  async deleteOrders() {
     this.orderService.deleteOrders();
   }
 
   @Get()
-  async getAllCats(@Query() query: string) {
+  async getAllOrders(@Query() query: string) {
     return await this.orderService.findAll(query);
   }
 }
