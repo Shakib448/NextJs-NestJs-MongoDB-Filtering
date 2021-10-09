8c36479b54a8fb6f3bb1dfae4b7c8838f975cc6b
Shakiba448 <shakiba448@gmail.com>
2021-10-09 22:16:33 +0600

    Done

--- client/pages/api/client.ts
+++ client/pages/api/client.ts
@@ -1,6 +1,5 @@
 import { create } from "apisauce";
 
-// const apiClient = create({ baseURL: "http://localhost:5000" });
-const apiClient = create({ baseURL: process.env.NEXT_PUBLIC_URI });
+const apiClient = create({ baseURL: "http://localhost:5000" });
 
 export default apiClient;
